b6414cc8b0467bc98d28dad3a0db34f218feb209
Matt Glick <matt.j.glick@gmail.com>
2015-04-01 00:34:12 -0400

    Body Parser and Form
    Can now submit a form, parse the body and retrieve variables from the
    front end.

--- server.js
+++ server.js
@@ -2,15 +2,32 @@
  * Created by MattGlick on 3/31/2015.
  */
 var express = require('express');
+var bodyParser = require('body-parser');
+
 
 var app = express();
 
 app.set('views', './views');
 app.set('view engine', 'jade');
 
+// handle the body
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// handle the directing of the bootstrap files
 app.use(express.static(__dirname + '/bower_components'));
 
+// get request to the index
 app.get('/', function(req, res) {
+    res.render('index', {
+        title: 'Welcome'
+    });
+});
+
+// posting to the index
+app.post('/', function (req, res) {
+    console.log(JSON.stringify(req.body, null, 2));
+
     res.render('index', {
         title: 'Welcome'
     });
